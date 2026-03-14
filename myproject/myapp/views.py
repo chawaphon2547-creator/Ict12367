49fecccb460624e396d43386edafe23583a1fa4b
chawaphon2547-creator <chawaphon2547@gmail.com>
2026-03-14 23:21:24 +0700

    Delete myproject directory

--- myproject/myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def contact(request):
-    return HttpResponse("<h1>ติดต่อ 68063241 ชวภณ รุ่งโรจน์</h1>")
-
-def form(request):
-    return render(request, 'form.html')
